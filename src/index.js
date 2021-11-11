fff14e8f3c403105e189ad87a3037865e6034705
FuadHashimov91 <fuadbh@code.edu.az>
2021-11-11 18:03:27 +0400

    Create Reducer and RootReducer && add React Router

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
- 
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { Provider } from "react-redux";
+import store from "./redux/store";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
- 
---
+++ src/redux/reducer.js
@@ -0,0 +1,14 @@
+const initialState = {
+  users: [],
+  user: {},
+  loading: false,
+};
+
+const usersReducers = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+export default usersReducers;
